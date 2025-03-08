d6f861a453559f34708564ae076b5598dd780981
Jesus-Puertos <226w0496@zongolica.tecnm.mx>
2025-03-08 17:59:29 -0600

    update: change info in mi portfolio and fix ancors

---
+++ src/consts/data-proyects.ts
@@ -0,0 +1,18 @@
+import Tecnologies from "@/components/Tecnologies.astro";
+
+const proyects = [
+
+    {
+        "title" : "Cadem",
+        "subtitle" : "Junio 2022",
+        "url" : "/images/proyecto-wix-cadem.webp",
+        "description" : "Sitio web hecho con Wix.",
+        Tecnologies : [
+            {
+                "name" : "Wix",
+                "url" : "https://www.wix.com/"
+            }
+        ]
+    }
+
+]
